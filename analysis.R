14b09966c16e1d544b406917487731ee5d0ff0c7
Andy <andypicke@gmail.com>
2023-08-07 15:36:36 -0600

    misc update to analysis script

--- analysis.R
+++ analysis.R
@@ -16,6 +16,13 @@ library(ggplot2)
 library(dplyr)
 
 
+sc <- 300
+if (sc !=200){
+  print('Warning, possible error in API call')
+  } else{
+    print('Status code 200')
+}
+
 # API key is stored in my .Renviron file
 api_key <- Sys.getenv("AFDC_KEY")
 
@@ -62,16 +69,6 @@ df %>% ggplot(aes(status_code)) + geom_bar()
 
 
 #
-# Plot cumulative sum of stations opened over time
-#
-df_opened %>% ggplot(aes(open_year,cumsum(nopened)))+
-  geom_line()+
-  geom_point()+
-  xlab("Year")+
-  ylab("# Stations")+
-  ggtitle("Cumulative sum of EV stations opened")
-
-#
 # Look at how many stations opened each year
 #
 
@@ -83,6 +80,18 @@ df_opened %>% ggplot(aes(open_year, nopened)) +
   ylab("# Stations Opened")+
   ggtitle('EV Stations Opened in Colorado Each Year')
 #
+
+
+#
+# Plot cumulative sum of stations opened over time
+#
+df_opened %>% ggplot(aes(open_year,cumsum(nopened)))+
+  geom_line()+
+  geom_point()+
+  xlab("Year")+
+  ylab("# Stations")+
+  ggtitle("Cumulative sum of EV stations opened")
+
 
 
 
@@ -131,3 +140,108 @@ df %>% group_by(facility_type) %>% summarize(n=n()) %>% arrange(desc(n)) %>% Vie
 df %>% group_by(city) %>% summarize(n=n()) %>% arrange(desc(n)) %>% View()
 
 
+#----------------------------
+
+
+#<!-- ## Break down by connector types, ev_connector_type,ev_charging_level -->
+  
+#  <!-- ```{r } -->
+  
+#   <!-- #unique(unlist(df$ev_connector_types)) -->
+#   
+#   <!-- df4 <- df3 %>% rowwise() %>%  -->
+#   
+#   <!--   mutate(hasJ=if_else("J1772" %in% ev_connector_types,1,0)) %>%  -->
+#   
+#   <!--   mutate(hasJcomb=if_else("J1772COMBO" %in% ev_connector_types,1,0)) %>%  -->
+#   
+#   <!--   mutate(hasChad=if_else("CHADEMO" %in% ev_connector_types,1,0)) %>%  -->
+#   
+#   <!--   mutate(hasTesla=if_else("TESLA" %in% ev_connector_types,1,0)) %>%  -->
+#   
+#   <!--   mutate(hasNema520=if_else("NEMA520" %in% ev_connector_types,1,0)) %>%  -->
+#   
+#   <!--   mutate(hasNema515=if_else("NEMA515" %in% ev_connector_types,1,0)) %>%  -->
+#   
+#   <!--   mutate(hasNema1450=if_else("NEMA1450" %in% ev_connector_types,1,0)) -->
+#   
+#   <!--   View(df4) -->
+#   
+#   <!-- df4 %>% -->
+#   
+#   <!--   select_if(is.numeric) %>% -->
+#   
+#   <!--   map_dbl(sum)   -->
+#   
+#   <!-- tibble(connector_type=names(df5),n=df5) %>% View() -->
+#   
+#   <!-- ``` -->
+#   
+#   <!-- ```{r } -->
+#   
+#   <!-- #df %>% count(ev_network) %>% arrange(desc(n)) %>% View() -->
+#   
+#   <!-- df %>%  count(ev_network) %>%  -->
+#   
+#   <!--   mutate(ev_network=as.factor(ev_network)) %>%  -->
+#   
+#   <!--   mutate(ev_network=forcats::fct_reorder(ev_network,n)) %>%  -->
+#   
+#   <!--   ggplot(aes(ev_network,n))+ -->
+#   
+#   <!--   geom_col()+ -->
+#   
+#   <!--   coord_flip()+ -->
+#   
+#   <!--   xlab("EV Network")+ -->
+#   
+#   <!--   ylab("# stations") -->
+#   
+#   <!-- ``` -->
+#   
+#   ## Break down by city/county
+#   
+#   ```{r}
+# 
+# df %>% count(city) %>%
+#   slice_max(order_by=n,n=15) %>%
+#   mutate(city=forcats::fct_reorder(city,n)) %>%
+#   ggplot(aes(city,n))+
+#   geom_col()+
+#   coord_flip()+
+#   ggtitle("Number of EV Stations by City (Top 15)")+
+#   xlab("City")+
+#   ylab("Number of stations")
+# 
+# #  View()
+# 
+# ```
+# 
+# 
+# 
+# Cumulative sum of chargers added by level
+# 
+# ```{r}
+# 
+# ev_opened_level %>% ggplot()+
+#   geom_line(aes(open_year,cumsum(n_DC)),col='black')+
+#   geom_line(aes(open_year,cumsum(n_L2)),col='red')+
+#   geom_line(aes(open_year,cumsum(n_L1)),col='green')
+# 
+# ```
+# 
+# ## Count \# individual plugs by level
+# 
+# ```{r }
+# 
+# n_lev1 <- sum(ev$ev_level1_evse_num,na.rm=TRUE)
+# n_lev2 <- sum(ev$ev_level2_evse_num,na.rm=TRUE)
+# n_dcfast <- sum(ev$ev_dc_fast_num, na.rm=TRUE)
+# 
+# plugs <- tibble(level=c('Level1','Level2','DC Fast'), n=c(n_lev1,n_lev2,n_dcfast))
+# 
+# plugs %>% ggplot(aes(level,n))+
+#   geom_col()+
+#   ggtitle("Total Number of Plugs By Charging Level")
+# 
+# ```
